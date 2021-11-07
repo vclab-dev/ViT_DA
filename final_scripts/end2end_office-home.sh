f4c80a0696f5719ee4449519d0782a7036f95be3
HiPatil <hipatil1998@gmail.com>
2021-11-08 00:40:40 +0530

    end to end script file

---
+++ final_scripts/end2end_office-home.sh
@@ -0,0 +1,7 @@
+python STDA_hp_cls_const_fbnm_with_grad.py --gpu_id 1 --s 0 --max_epoch 30 --interval 30 --batch_size 64 --dset office-home --net deit_s --wandb 1 --fbnm_par 4.0 --grad_norm 1 --output_src src_train --output STDA_wt_fbnm_with_grad_rlcc_soft_with_stg --suffix fbnm_with_grad_rlcc_soft_with_stg --rlcc --soft_pl
+
+python STDA_hp_cls_const_fbnm_with_grad.py --gpu_id 1 --s 0 --max_epoch 30 --interval 30 --batch_size 64 --dset office-home --net deit_s --wandb 1 --fbnm_par 4.0 --grad_norm 1 --output_src src_train --output STDA_wt_fbnm_with_grad_rlcc_soft_with_stg --suffix fbnm_with_grad_rlcc_soft_with_stg --rlcc --soft_pl
+
+python STDA_hp_cls_const_fbnm_with_grad.py --gpu_id 1 --s 0 --max_epoch 30 --interval 30 --batch_size 64 --dset office-home --net deit_s --wandb 1 --fbnm_par 4.0 --grad_norm 1 --output_src src_train --output STDA_wt_fbnm_with_grad_rlcc_soft_with_stg --suffix fbnm_with_grad_rlcc_soft_with_stg --rlcc --soft_pl
+
+python STDA_hp_cls_const_fbnm_with_grad.py --gpu_id 1 --s 0 --max_epoch 30 --interval 30 --batch_size 64 --dset office-home --net deit_s --wandb 1 --fbnm_par 4.0 --grad_norm 1 --output_src src_train --output STDA_wt_fbnm_with_grad_rlcc_soft_with_stg --suffix fbnm_with_grad_rlcc_soft_with_stg --rlcc --soft_pl
